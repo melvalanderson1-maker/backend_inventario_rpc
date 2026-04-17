88016576cd94d6ef21bf8021dccf06a8f603ba4d
melvalanderson1-maker <melvalanderson1@gmail.com>
2026-04-17 10:03:19 -0500

    agregarndo x session token en server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization", "x-session-token"]
 };
 
 app.use(cors(corsOptions));
